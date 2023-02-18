2b01b60297c538c74a77d1d6faf53621c9d8516c
Bashiru200 <bashirubarrie12@gmail.com>
2023-02-18 04:41:51 -0800

    printf funtion

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main()
+/**
+*main - entry point of executable code
+*
+* Return: this program return 0
+*/
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
-	return 0;
+	return (0);
 }
