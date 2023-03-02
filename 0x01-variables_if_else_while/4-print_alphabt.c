568f9ce531014c7875b08ac711c19892fbd94727
Bashiru200 <bashirubarrie12@gmail.com>
2023-03-02 05:20:38 -0800

    When I was having that alphabet soup, I never thought that it would pay off When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - the main entry point
+ * Return: return value 0
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n < 122)
+	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
