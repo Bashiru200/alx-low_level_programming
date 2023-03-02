7513c5acd1ab7d303da9383dc9d8040325aac5bd
Bashiru200 <bashirubarrie12@gmail.com>
2023-03-02 06:13:56 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - data entry point
+ * Return : return value 0
+ */
+int main(void)
+{
+	int sd = 48;
+
+	while (sd <= 57)
+	{
+		putchar(sd);
+		sd++;
+	}
+	putchar('\n');
+	return (0);
+}
